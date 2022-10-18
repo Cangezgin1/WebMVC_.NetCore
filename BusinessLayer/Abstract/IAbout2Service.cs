d7709431551da61348d947d82ac428b4ea74338f
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-10-18 16:48:48 +0300

    Add About2Service and About2Manager

--- BusinessLayer/Abstract/IAbout2Service.cs
+++ BusinessLayer/Abstract/IAbout2Service.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-    public interface IAbout2Service : IGenericService<About>
+    public interface IAbout2Service : IGenericService<About2>
     {
     }
 }
---
+++ BusinessLayer/Concrete/About2Manager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class About2Manager : IAbout2Service
+    {
+        IAbout2Dal _about2Dal;
+
+        public About2Manager(IAbout2Dal about2Dal)
+        {
+            _about2Dal = about2Dal;
+        }
+
+        public void TAdd(About2 p)
+        {
+            _about2Dal.Insert(p);
+        }
+
+        public void TDelete(About2 p)
+        {
+            _about2Dal.Delete(p);
+        }
+
+        public About2 TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About2> TGetList(About2 p)
+        {
+            return _about2Dal.GetList();
+        }
+
+        public void TUpdate(About2 p)
+        {
+            _about2Dal.Update(p);
+        }
+    }
+}
