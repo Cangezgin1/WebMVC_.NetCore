715ea3172613868912c18b1f007e86b6058eaaec
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-10-21 17:41:06 +0300

    Add Manager, Partial(Component) and cshtml.

---
+++ Travelsal-.NetCore/ViewComponents/Default/_FeaturePartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travelsal_.NetCore.ViewComponents.Default
+{
+    public class _FeaturePartial:ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
